9ec0232323f2fe9f192e374047788e4d452aad0b
Okonu <ianyakundi015@gmail.com>
2022-11-16 01:09:24 +0300

    app users

---
+++ app/RegisterAgent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RegisterAgent extends Model
+{
+    public function jazzid()
+    {
+        return $this->hasOne(JazzId::class, 'agent_id_fk', 'id');
+    }
+
+    public function post()
+    {
+        return $this->hasMany(AgentPost::class, 'advisor_id_fk', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id_fk', 'id');
+    }
+}
