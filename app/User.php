793c8447959a194e8a36f8e56ea1ef49df0e1c96
Okonu <ianyakundi015@gmail.com>
2022-11-17 00:31:04 +0300

    user file edits

--- app/User.php
+++ app/User.php
@@ -6,15 +6,49 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
     /**
      * The attributes that are mass assignable.
-     * 
+     *
      * @var array
-     * /
+     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function tenantrelation()
+    {
+        return $this->hasOne(RegisterTenant::class, 'user_id_fk', 'id');
+    }
+    public function adminrelation()
+    {
+        return $this->hasOne(RegisterAdmin::class, 'user_id_fk', 'id');
+    }
+    public function agentrelation()
+    {
+        return $this->hasOne(RegisterAgent::class, 'user_id_fk', 'id');
+    }
+
+
+    public function user_1_conversation()
+    {
+        return $this->hasMany(Conversation::class, 'user_1');
+    }
+
+    public function user_2_conversation()
+    {
+        return $this->hasMany(Conversation::class, 'user_2');
+    }
 }
