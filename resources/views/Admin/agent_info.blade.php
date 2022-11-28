cb59958ff85481f0e5482595882d849945e4218b
Okonu <ianyakundi015@gmail.com>
2022-11-28 17:49:49 +0300

    page edits

---
+++ resources/views/Admin/agent_info.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kodisha Online House Rental</title>
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" />
+    <style>
+        html,
+        body {
+            font-family: 'Open Sans', sans-serif;
+            height: 100%;
+        }
+
+        body {
+            background: #fff;
+            height: 100;
+        }
+    </style>
+</head>
