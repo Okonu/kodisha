8199b1d0823c126db2b3d9c0d9e74d6f6b319943
Okonu <ianyakundi015@gmail.com>
2023-01-15 23:08:31 +0300

    missing class: SystemRoles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(class: [
-            SystemRoles::class,
+        $this->call([
+            SystemRoles::class,s
             UserTableSeeder::class,
             TenantTableSeeder::class,
             AdminTableSeeder::class,
